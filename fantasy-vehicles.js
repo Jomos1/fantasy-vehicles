3637a7bd098d118797b29c714615c09e1167b96d
Jomos <jomarijacinto45@gmail.com>
2017-08-30 10:26:12 -1000

    modified:   fantasy-vehicles.js

--- fantasy-vehicles.js
+++ fantasy-vehicles.js
@@ -1,40 +1,58 @@
 class Vehicle {
 	constructor(make, color, type){
-		this.make = make;
-		this.color = color;
-		this.type = type;
+		this._make = make;
+		this._color = color;
+		this._type = type;
 	}
 
 	get vehicleDetails(){
 		return `Your Vehicle is made by ${this.make}, and is ${this.color}, and least importantly it's a ${this.type}`	
 	}
 	set vehicleColor(color){
-		this.color = "Hot Pink"
+		if(typeof color === 'string'){
+		this._color = "Hot Pink";
+		}else{
+			throw new TypeError('Not a string')
+		}
 	}
 	set vehicleMake(make){
-		this.make = "Audi"
+		if(typeof make === 'string'){
+		this._make = "Audi";
+		}else{
+			throw new TypeError('Not a string')
+		}
 	}
 	set vehicleType(type){
-		this.type = "Bugatti"
+		if(typeof type === 'string'){
+		this._type = "Bugatti";
+	}else{
+		throw new TypeError('Not s string dumbass')
+	}
 	}
 } 
-class SuperCar  extends Vehicle {
+class SuperCar extends Vehicle {
 	constructor(make,color,type,topSpeed,cost){
 		super(make,color,type);
-		this.topSpeed = make;
-		this.cost = cost;
+		this._topSpeed = make;
+		this._cost = cost;
 	}
 	set vehicleTopSpeed(topSpeed){
-		this.topSpeed = 254;
+		this._topSpeed = '254';
 	}
 	set vehicleCost(cost){
-		this.cost = 1700000;
+		this._cost = '1700000';
 	}
-	get superCarDetails{
+	get superCarDetails() {
 		return `Your Vehicle is made by ${this.make}, and is ${this.color}, and least importantly it's a ${this.type} its top speed is ${this.topSpeed} and costs ${this.cost}`
 	}
 }
 
+class Motorcycle extends Vehicle{
+	constructor(make,color,type,topSpeed,cost){
+		super(make,color,type,topSpeed,cost)
+	}
+}
+
 let jank = new Vehicle;
-let jank2 = new SuperCar('Audi', 'Hot Pink', 'Bugatti',254,1700000);
+let jank2 = new SuperCar('Audi', 'Hot Pink', 'Bugatti','254','1700000');
 console.log(Vehicle.SuperCar);
