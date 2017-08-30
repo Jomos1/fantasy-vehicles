1d5f32938cbd7d414f8736d9b4bee7cd4651d0f7
Jomos <jomarijacinto45@gmail.com>
2017-08-30 10:11:45 -1000

    modified:   fantasy-vehicles.js

--- fantasy-vehicles.js
+++ fantasy-vehicles.js
@@ -9,7 +9,32 @@ class Vehicle {
 		return `Your Vehicle is made by ${this.make}, and is ${this.color}, and least importantly it's a ${this.type}`	
 	}
 	set vehicleColor(color){
-		this.color;
+		this.color = "Hot Pink"
+	}
+	set vehicleMake(make){
+		this.make = "Audi"
+	}
+	set vehicleType(type){
+		this.type = "Bugatti"
 	}
 } 
-console.log(Vehicle.vehicle);
+class SuperCar  extends Vehicle {
+	constructor(make,color,type,topSpeed,cost){
+		super(make,color,type);
+		this.topSpeed = make;
+		this.cost = cost;
+	}
+	set vehicleTopSpeed(topSpeed){
+		this.topSpeed = 254;
+	}
+	set vehicleCost(cost){
+		this.cost = 1700000;
+	}
+	get superCarDetails{
+		return `Your Vehicle is made by ${this.make}, and is ${this.color}, and least importantly it's a ${this.type} its top speed is ${this.topSpeed} and costs ${this.cost}`
+	}
+}
+
+let jank = new Vehicle;
+let jank2 = new SuperCar('Audi', 'Hot Pink', 'Bugatti',254,1700000);
+console.log(Vehicle.SuperCar);
