851504d0fe4285863266c53b77d7f05f3a292aaf
Jomos <jomarijacinto45@gmail.com>
2017-08-30 10:32:40 -1000

    modified:   fantasy-vehicles.js

--- fantasy-vehicles.js
+++ fantasy-vehicles.js
@@ -6,27 +6,27 @@ class Vehicle {
 	}
 
 	get vehicleDetails(){
-		return `Your Vehicle is made by ${this.make}, and is ${this.color}, and least importantly it's a ${this.type}`	
+    return `Your vehicle is a ${this._type}, made by ${this._make}, and is ${this._color}.`;	
 	}
 	set vehicleColor(color){
 		if(typeof color === 'string'){
 		this._color = "Hot Pink";
 		}else{
-			throw new TypeError('Not a string')
+			throw new TypeError('Not a string');
 		}
 	}
 	set vehicleMake(make){
 		if(typeof make === 'string'){
 		this._make = "Audi";
 		}else{
-			throw new TypeError('Not a string')
+			throw new TypeError('Not a string');
 		}
 	}
 	set vehicleType(type){
 		if(typeof type === 'string'){
 		this._type = "Bugatti";
 	}else{
-		throw new TypeError('Not s string dumbass')
+		throw new TypeError('Not s string dumbass');
 	}
 	}
 } 
@@ -40,19 +40,43 @@ class SuperCar extends Vehicle {
 		this._topSpeed = '254';
 	}
 	set vehicleCost(cost){
-		this._cost = '1700000';
+		if(typeof cost === 'number'){
+		this._cost = 1700000;
+	}else{
+		throw new TypeError('not a number...baka');
+	}
 	}
 	get superCarDetails() {
-		return `Your Vehicle is made by ${this.make}, and is ${this.color}, and least importantly it's a ${this.type} its top speed is ${this.topSpeed} and costs ${this.cost}`
+			return `Your super car is a ${this._type}, made by ${this._make}, and is ${this._color} with a top speed of ${this._topSpeed} mph and total cost of ${this._cost} million.`
 	}
 }
 
 class Motorcycle extends Vehicle{
-	constructor(make,color,type,topSpeed,cost){
-		super(make,color,type,topSpeed,cost)
-	}
-}
+  constructor(make, color, type, topSpeed, cost){
+    super(make, color, type);
+    this._topSpeed = topSpeed;
+    this._cost = cost;
+  }
 
+  get bikeDetails(){
+    return `Your motorcycle is a ${this._type}, made by ${this._make}, and is ${this._color} with a top speed of ${this._topSpeed} mph and total cost of ${this._cost} dollars.`
+  }
+
+  set bikeSpeed(topSpeed){
+    if(typeof topSpeed === 'number'){
+      this._topSpeed = 200;
+    }else{
+      throw new TypeError('Motorcycle.topSpeed is not a number');
+    }
+  }
+  set bikeCost(cost){
+  	if(typeof cost === 'number'){
+  		this._cost = 20000;
+  	}else{
+  		throw new TypeError('Motorcycle.cost is not a number...baka');
+  	}
+  }
+}
 let jank = new Vehicle;
 let jank2 = new SuperCar('Audi', 'Hot Pink', 'Bugatti','254','1700000');
 console.log(Vehicle.SuperCar);
